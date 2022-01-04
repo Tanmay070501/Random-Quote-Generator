4c0796427a983f5e4a39f31fd81875762af15639
Tanmay Maheshwari <94776696+Tanmay070501@users.noreply.github.com>
2022-01-04 20:26:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const quote =  document.getElementById('quote');
+const author = document.querySelector('.author')
+/*----- Buttons -----*/
+const newQuoteBtn = document.querySelector('.new-btn');
+
+
+function showNewQuote(text,athr){
+    quote.textContent = text;
+    author.textContent = '-' + athr;
+}
+
+async function newQuote(){
+    try{
+        const quote = await fetch("https://api.quotable.io/random");
+        const ctnt = await quote.json();//content from the api
+        showNewQuote(ctnt.content,ctnt.author);
+    }catch(err){
+        //err
+        console.error(err);
+    }
+}
+
+newQuoteBtn.addEventListener('click',()=>{
+    newQuote();
+})
